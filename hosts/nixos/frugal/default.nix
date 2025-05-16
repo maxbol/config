340f696ac5354ef0b989739a82d4c9bfe941169d
Max Bolotin <maks.bolotin@gmail.com>
2025-05-16 10:04:15 +0200

    Temporarily comment out desktop-config

--- hosts/nixos/frugal/default.nix
+++ hosts/nixos/frugal/default.nix
@@ -16,7 +16,7 @@
   features.application-config.enable = true;
   features.core-services.enable = true;
   features.custom-udev-rules.enable = true;
-  features.desktop-config.enable = true;
+  # features.desktop-config.enable = true;
   features.localisation.enable = true;
   features.secrets-management.enable = true;
   features.system-start = {
