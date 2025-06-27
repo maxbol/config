5794be6b6124af2d6ebf55a9d9f0eaf04187afd5
Max Bolotin <maks.bolotin@gmail.com>
2025-06-27 20:49:59 +0200

    remove unused config/ files

--- config/i3/scripts/startup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-kitty &
-
-tmux setenv -g HYPRLAND_INSTANCE_SIGNATURE "$HYPRLAND_INSTANCE_SIGNATURE"
-
-$HOME/.config/polybar/launch.sh &
--- config/sway/scripts/startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-tmux setenv -g HYPRLAND_INSTANCE_SIGNATURE "$HYPRLAND_INSTANCE_SIGNATURE"
-
-wl-paste --type text --watch cliphist store &
-wl-paste --type image --watch cliphist store &
-
-waybar &
-kitty &
